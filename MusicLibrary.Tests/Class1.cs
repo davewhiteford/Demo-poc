41324943c0ecf77b7e508b50c08c3db8f32aa035
Dave Whiteford <dave.whiteford+privategithub@gmail.com>
2021-10-14 22:55:08 +0100

    pulled in nUnit and wrote a simple test

--- MusicLibrary.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MusicLibrary.Tests
-{
-    public class Class1
-    {
-    }
-}
---
+++ MusicLibrary.Tests/PlainTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MusicLibrary.Tests
+{
+    [TestFixture]
+    public class PlainTest
+    {
+        [Test]
+        public void IsTrue_Yes() 
+        {
+            var isTrue = true;
+            Assert.AreEqual(isTrue, true);
+        }
+    }
+}
